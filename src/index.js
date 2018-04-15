2efdac64f681bba648c20fa8fa8298cc3fbe5244
Kevin Lin <kevinptt0323@gmail.com>
2018-04-15 23:04:57 +0800

    add support to twitch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import componentHandler from 'componentHandler';
 import GarenaLive from './sites/garena-live';
+import Twitch from './sites/twitch-tv';
 
-const chatroom = new GarenaLive();
+const { host } = document.location;
+const hostMapping = [
+    { validate: /^garena\.live$/, chatroom: GarenaLive },
+    { validate: /^www\.twitch\.tv$/, chatroom: Twitch },
+];
 
-chatroom.subscribe((msg) => {
-    chatroom.render(msg);
-});
+const Chatrooms = hostMapping
+    .filter(m => m.validate.test(host))
+    .map(m => m.chatroom);
+
+if (Chatrooms.length >= 1) {
+    const chatroom = new Chatrooms[0]();
+
+    chatroom.subscribe((msg) => {
+        chatroom.render(msg);
+    });
+}
 
 componentHandler.upgradeAllRegistered();
---
+++ src/sites/twitch-tv/index.js
@@ -0,0 +1,34 @@
+import Chatroom from '../../chatroom';
+
+class Twitch extends Chatroom {
+    constructor() {
+        super();
+        setTimeout(() => {
+            const chatroomEl =
+                document.querySelector('.chat-list__lines div[role="log"]');
+            this.initDomObserver(chatroomEl);
+            const parentEl = document.querySelector('.player-video');
+            this.canvasEl = Chatroom.createCanvas(parentEl);
+        }, 2000);
+    }
+
+    onUpdate(mutations) {
+        mutations.forEach(({ addedNodes }) => {
+            addedNodes.forEach((node) => {
+                const authorEl =
+                    node.querySelector('.chat-author__display-name');
+                const contentEl =
+                    node.querySelector('span[data-a-target="chat-message-text"]');
+                if (authorEl && contentEl) {
+                    this.msgObserver.next({
+                        author: authorEl.innerHTML,
+                        color: authorEl.style.color,
+                        content: contentEl.innerHTML,
+                    });
+                }
+            });
+        });
+    }
+}
+
+export default Twitch;
