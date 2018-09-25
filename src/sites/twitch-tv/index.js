afe589e4e815f5cb0a1a028baa05c3d40e282385
Kevin Lin <kevinptt0323@gmail.com>
2018-09-25 17:54:31 +0800

    [Twitch] Show danmaku in theater mode

--- src/sites/twitch-tv/index.js
+++ src/sites/twitch-tv/index.js
@@ -10,9 +10,38 @@ class Twitch extends Chatroom {
   initialize = () => {
     const chatroomEl = $('.chat-list__lines div[role="log"]');
     this.initDomObserver(chatroomEl);
-    const parentEl = $('.player-video');
-    this.canvasEl = Chatroom.createCanvas(parentEl);
+    const playerEl = $('.player-video');
+    this.canvasEl = Chatroom.createCanvas(playerEl);
+
+    const oldToggleChatroomBtn = $('button[data-a-target="right-column__toggle-collapse-btn"]');
+    const newToggleChatroomBtn = oldToggleChatroomBtn.cloneNode(true);
+    newToggleChatroomBtn.addEventListener('click', this.toggleChatroom);
+    oldToggleChatroomBtn.parentNode.appendChild(newToggleChatroomBtn);
+    oldToggleChatroomBtn.remove();
   };
+
+  toggleChatroom() {
+    const chatroomRootEl = $('.right-column > div:first-child');
+    const whispersEl = $('.whispers');
+    const playerRootEl = $('.persistent-player');
+    const oldExpanded = chatroomRootEl.classList.contains('tw-block');
+    const isTheatre = playerRootEl.classList.contains('persistent-player--theatre');
+    if (oldExpanded) {
+      chatroomRootEl.classList.remove('tw-block');
+      chatroomRootEl.classList.add('tw-hide');
+      whispersEl.classList.remove('whispers--right-column-expanded');
+      if (isTheatre) {
+        playerRootEl.style['width'] = '100%';
+      }
+    } else {
+      chatroomRootEl.classList.add('tw-block');
+      chatroomRootEl.classList.remove('tw-hide');
+      whispersEl.classList.add('whispers--right-column-expanded');
+      if (isTheatre) {
+        playerRootEl.style['width'] = 'calc(100% - 34rem)';
+      }
+    }
+  }
 
   onUpdate(mutations) {
     mutations.forEach(({ addedNodes }) => {
